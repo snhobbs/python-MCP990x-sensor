340315591194cce8f28e209c970a028e4872d02a
simon <simon.hobbs@electroooptical.net>
2022-04-13 21:27:16 -0400

    adding readout of all channels

---
+++ example/mcp9904_read_all_channels.py
@@ -0,0 +1,32 @@
+from mcp990x import Sensor
+from time import sleep
+import random
+import time
+import numpy as np
+import rich
+from rich.live import Live
+from rich.table import Table
+
+
+def read_sensor(channel):
+    return s.read(channel)
+
+def generate_table() -> Table:
+    """Make a new table."""
+    table = Table(title="Temperatures (C)", box=rich.box.MINIMAL_DOUBLE_HEAD)
+    for channel in range(4):
+        table.add_column("   %d   "%channel)
+
+    row = []
+    for channel in range(4):
+        reading = read_sensor(channel)
+        row.append(reading)
+    table.add_row(*np.array(row, dtype=str))
+    return table
+
+
+s = Sensor(1, i2c_addr=0x3c, debug=False)
+with Live(generate_table(), refresh_per_second=4) as live:
+    while True:
+        time.sleep(2)
+        live.update(generate_table())
--- src/mcp990x/mcp990x.py
+++ src/mcp990x/mcp990x.py
@@ -3,6 +3,71 @@
 """This module allows driving the I2C temp sensor"""
 import smbus
 
+class Temperature:
+    def __init__(self, value, unit="celsius"):
+        k = value
+        if unit.lower() in ["celsius", "c"]:
+            k = self.celsius_to_kelvin(value)
+        elif unit.lower() in ["fahrenheit", "f"]:
+            k = self.fahrenheit_to_kelvin(value)
+        elif unit.lower() in ["kelvin", "k"]:
+            pass
+        else:
+            raise ValueError("unknown unit %s", str(unit))
+        self._kelvin = k
+
+    def __repr__(self):
+        return str(self.celsius)
+
+    def kelvin_to_celsius(self, value):
+        return value - 273.15
+
+    def kelvin_to_fahrenheit(self, value):
+        return 1.8*self.kelvin_to_celsius(value) + 32
+
+    def celsius_to_kelvin(self, value):
+        return value + 273.15
+
+    def fahrenheit_to_kelvin(self, value):
+        return (value - 32)/1.8
+
+    @property
+    def k(self):
+        return self._kelvin
+
+    @property
+    def kelvin(self):
+        return self._kelvin
+
+    @property
+    def c(self):
+        return self.celsius
+
+    @property
+    def celsius(self):
+        return self.kelvin_to_celsius(self.kelvin)
+
+    @property
+    def f(self):
+        return self.fahrenheit
+
+    @property
+    def fahrenheit(self):
+        return self.kelvin_to_fahrenheit(self.kelvin)
+
+assert(Temperature(0, "c").fahrenheit == 32)
+assert(Temperature(0, "k").c == -273.15)
+assert(Temperature(0, "c").k == 273.15)
+
+def reading_to_temperature(arr) -> Temperature:
+    # stitch together the temp reading bytes: the total data is 11 bits,
+    # with the most significant 8 in the high reg and the least significant 3
+    # in the top 3 bits of the low reg. So, OR them together as a 17 bits integer,
+    # then shift everything right by 5
+    # arr[1] = arr[1]>>5
+    # temp = int.from_bytes(arr, byteorder="big", signed=True) + 0.125
+    temp = ((arr[0] << 8 | arr[1]) >> 5) * 0.125
+    return Temperature(temp, "c")
 
 class Sensor(object):
     """Sensor([bus]) -> Sensor
@@ -11,7 +76,18 @@ class Sensor(object):
     """
     REG_ADDR_TEMP_HIGH = 0x00
     REG_ADDR_TEMP_LOW = 0x29
+
+    REG_ADDR_EXT1_TEMP_HIGH = 0x01
+    REG_ADDR_EXT1_TEMP_LOW = 0x10
+
+    REG_ADDR_EXT2_TEMP_HIGH = 0x23
+    REG_ADDR_EXT2_TEMP_LOW = 0x24
+
+    REG_ADDR_EXT3_TEMP_HIGH = 0x2a
+    REG_ADDR_EXT3_TEMP_LOW = 0x2b
+
     REG_ADDR_LAST = 0xFF
+
     _bus = None
     _debug = False
     _i2c_addr = 0b1001100
@@ -58,22 +134,36 @@ class Sensor(object):
 
         return self._bus.read_byte_data(self._i2c_addr, reg_addr)
 
-    def read(self):
+    def read(self, channel=0):
         """read()
         read the current temperature
         """
-        raw_temp_reg_low = self.__read_register(self.REG_ADDR_TEMP_LOW)
-        raw_temp_reg_high = self.__read_register(self.REG_ADDR_TEMP_HIGH)
+        if channel == 0:
+            data = [self.__read_register(self.REG_ADDR_TEMP_HIGH), self.__read_register(self.REG_ADDR_TEMP_LOW)]
+        elif channel == 1:
+            data = [
+                self.__read_register(self.REG_ADDR_EXT1_TEMP_HIGH),
+                self.__read_register(self.REG_ADDR_EXT1_TEMP_LOW)]
+        elif channel == 2:
+            data = [
+                self.__read_register(self.REG_ADDR_EXT2_TEMP_HIGH),
+                self.__read_register(self.REG_ADDR_EXT2_TEMP_LOW)]
+        elif channel == 3:
+            data = [
+                self.__read_register(self.REG_ADDR_EXT3_TEMP_HIGH),
+                self.__read_register(self.REG_ADDR_EXT3_TEMP_LOW)]
+        else:
+            raise ValueError("Unknown channel %s", channel)
 
         # stitch together the temp reading bytes: the total data is 11 bits,
         # with the most significant 8 in the high reg and the least significant 3
         # in the top 3 bits of the low reg. So, OR them together as a 17 bits integer,
         # then shift everything right by 5
-        raw_temp_reg = ((raw_temp_reg_high << 8) | raw_temp_reg_low) >> 5
         if self._debug:
-            print("reg: " + hex(raw_temp_reg))
+            for reg in data:
+                print("reg: " + hex(reg))
 
-        temp = raw_temp_reg * 0.125
+        temp = reading_to_temperature(data)
         if self._debug:
             print(str(temp))
 
